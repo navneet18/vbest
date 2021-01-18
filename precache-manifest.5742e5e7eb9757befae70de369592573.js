c2bc019d4d85e40ff11e69ad8ac92120f3a8c103
Navneet <navgpt18@gmail.com>
2021-01-19 00:55:15 +0530

    Updates

---
+++ precache-manifest.5742e5e7eb9757befae70de369592573.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37669278796890bb8f965d29ea215ac5",
+    "url": "/vbest/index.html"
+  },
+  {
+    "revision": "d7e4987bf8abdd59cc33",
+    "url": "/vbest/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5eed82cae8a0404ad757",
+    "url": "/vbest/static/css/main.9da08b7d.chunk.css"
+  },
+  {
+    "revision": "d7e4987bf8abdd59cc33",
+    "url": "/vbest/static/js/2.27a79197.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vbest/static/js/2.27a79197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eed82cae8a0404ad757",
+    "url": "/vbest/static/js/main.b2eeefe3.chunk.js"
+  },
+  {
+    "revision": "62f4deb073aa3f1c8830",
+    "url": "/vbest/static/js/runtime-main.f8b5f3c1.js"
+  },
+  {
+    "revision": "a59ce4af5085b58246d8e535f4c9192d",
+    "url": "/vbest/static/media/basa_fish1.a59ce4af.jpg"
+  },
+  {
+    "revision": "88d8352bc7fbc7981822bb02eba5ec50",
+    "url": "/vbest/static/media/bhetki_fish2.88d8352b.jpg"
+  },
+  {
+    "revision": "aeee37d6e5eb2e2f8fe8e1ce28b529bb",
+    "url": "/vbest/static/media/catla_fish.aeee37d6.jpg"
+  },
+  {
+    "revision": "8fee4502f966daf1f0d45737d5b8d427",
+    "url": "/vbest/static/media/cimage2.8fee4502.jpeg"
+  },
+  {
+    "revision": "1c4d05bcbe0d2b3691811d22e96f82b8",
+    "url": "/vbest/static/media/logo.1c4d05bc.jpeg"
+  },
+  {
+    "revision": "905fdbe5aa3f5a654746bf8e45d5eaf0",
+    "url": "/vbest/static/media/prawns1.905fdbe5.jpg"
+  },
+  {
+    "revision": "dbe50067ce22f16be7e8c379c6781193",
+    "url": "/vbest/static/media/rohu_fish.dbe50067.jpg"
+  }
+]);
